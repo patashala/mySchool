5968c0f6c8884a0b51efd6bc928786048d734df8
abrahamgolla17 <abrahamg@zetagile.com>
2016-11-25 07:34:13 +0530

    update

--- webservices/src/com/myschool/project/dto/school/ClassSubject.java
+++ webservices/src/com/myschool/project/dto/school/ClassSubject.java
@@ -1,15 +1,13 @@
 package com.myschool.project.dto.school;
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.myschool.project.dto.common.EducationBoard;
 
 @Entity
 @Table(name="ps_classsub_tbl")
@@ -18,7 +16,70 @@ public class ClassSubject implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	
-	@ManyToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
-	@JoinTable(name="ps_school_subjects", joinColumns={@JoinColumn(name="schoolId")})
-	private List<School> schools;
+	/*@ManyToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinTable(name="ps_school_subjects", joinColumns={@JoinColumn(name="schoolId")},inverseJoinColumns={@JoinColumn(name="subjectId")})
+	private List<School> schools;*/
+	
+	@ManyToOne
+	private EducationBoard boardId;
+	
+	private SchoolBranch branch;
+	
+	private SchoolBranchClass branchClass;
+	
+	private SchoolSubject subjectId;
+	
+	@Column(name="desc")
+	private String description;
+	
+	private SchoolBranchClassSections sectionId;
+
+	public EducationBoard getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(EducationBoard boardId) {
+		this.boardId = boardId;
+	}
+
+	public SchoolBranch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(SchoolBranch branch) {
+		this.branch = branch;
+	}
+
+	public SchoolBranchClass getBranchClass() {
+		return branchClass;
+	}
+
+	public void setBranchClass(SchoolBranchClass branchClass) {
+		this.branchClass = branchClass;
+	}
+
+	public SchoolSubject getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(SchoolSubject subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public SchoolBranchClassSections getSectionId() {
+		return sectionId;
+	}
+
+	public void setSectionId(SchoolBranchClassSections sectionId) {
+		this.sectionId = sectionId;
+	}
+	
 }
